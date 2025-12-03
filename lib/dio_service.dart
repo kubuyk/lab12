2a090c48d491b2b1ded54c6a2b9e665500f908df
Bogdan <kubyuk.b1504@lntu.edu.ua>
2025-12-03 12:49:16 +0200

    .

--- lib/dio_service.dart
+++ lib/dio_service.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+
+
 class DioService {
 
   static const String baseUrl = 'http://lab12_test_lntu.requestcatcher.com';
   final Dio _dio = Dio();
+
+  DioService() {
+    (_dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+      final client = HttpClient();
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
+  }
 
   Future<void> sendData(String endpoint, Map<String, dynamic> data) async {
     try {
@@ -14,3 +26,4 @@ class DioService {
     }
   }
 }
+
